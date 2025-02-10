90684600b693ff0b0118ddf894c35509c1870f44
LilianaAmangoua <amangoua99@gmail.com>
2025-02-10 16:29:40 +0100

    Initial commit

---
+++ src/main/java/com/cart_page/cart_page/CartPageApplication.java
@@ -0,0 +1,13 @@
+package com.cart_page.cart_page;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartPageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CartPageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cart_page/cart_page/CartPageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cart_page.cart_page;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartPageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
